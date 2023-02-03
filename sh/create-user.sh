1d90819599fbb00088aeeb00457499710cb77b7e
xethhung <adamhung12@gmail.com>
2023-02-03 10:41:13 +0800

    final

---
+++ sh/create-user.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mongosh mongodb://mongo.example.com:27017 --tls \
+  --tlsCAFile /data/ssl/mongo-client-ca.pem \
+  --tlsCertificateKeyFile /data/ssl/mongo-client-key.pem <<EOF
+use admin
+db.createUser({user:"root",pwd:"Abcd1234",roles:[{role:"userAdminAnyDatabase",db:"admin"}]})
+db.auth("root", "Abcd1234")
+db.createUser({user:"user",pwd:"Abcd1234",roles:[{role:"readWrite",db:"testDB"}]})
+EOF
